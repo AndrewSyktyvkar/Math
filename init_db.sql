9894358971fb66b68f817be467573ab03eae8e52
muller95 <muller95@yandex.ru>
2017-04-05 11:38:03 -0700

    Auto increment for primary key

--- init_db.sql
+++ init_db.sql
@@ -3,14 +3,14 @@ CREATE DATABASE math CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE math;
 
 create table users ( 
-    user_id int, 
+    user_id int PRIMARY KEY AUTO_INCREMENT, 
     user_name text, 
     user_passwort_hash text,
     access_token text
 );
 
 create table authors ( 
-    author_id int, 
+    author_id int PRIMARY KEY AUTO_INCREMENT, 
     author_name text, 
     author_passwort_hash text,
     access_token text
@@ -21,24 +21,24 @@ create table invites (
 );
 
 create table subjects (
-	subject_id int,
+	subject_id int PRIMARY KEY AUTO_INCREMENT,
 	subject_name text
 );
 	
 create table categories (
-	category_id int,
+	category_id int PRIMARY KEY AUTO_INCREMENT,
 	category_name text,
 	subject_id int
 );
 
 create table subcategories (
-	subcategory_id int,
+	subcategory_id int PRIMARY KEY AUTO_INCREMENT,
 	subcategory_name text,
 	category_id int	
 );
 	
 create table tasks (
-	task_id int,
+	task_id int PRIMARY KEY AUTO_INCREMENT,
 	task_name text,
 	task_text text,
 	task_rate int,
@@ -47,7 +47,7 @@ create table tasks (
 );
 
 create table tests (
-	test_id int,
+	test_id int PRIMARY KEY AUTO_INCREMENT,
 	test_description text,
 	test_date date,
 	test_rate int,
@@ -55,27 +55,27 @@ create table tests (
 );
 	
 create table tasks_in_tests (
-	test_id int,
+	test_id int PRIMARY KEY AUTO_INCREMENT,
 	task_id int,
 	task_number int
 );
 	
 create table articles (
-	article_id int,
+	article_id int PRIMARY KEY AUTO_INCREMENT,
 	article_text text,
 	article_date date,
 	subcategory_id int
 );
 	
 create table solutions (
-	solution_id int,
+	solution_id int PRIMARY KEY AUTO_INCREMENT,
 	solution text,
 	answer text,
 	task_id int
 );
 	
 create table comments (
-	comment_id int,
+	comment_id int PRIMARY KEY AUTO_INCREMENT,
 	comment_to text,
 	comment_text text,
 	comment_date date,
