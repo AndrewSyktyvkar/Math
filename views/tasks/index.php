a3002393eb5aed018c5f88a87569f9dc63ee8657
muller95 <muller95@yandex.ru>
2017-04-12 23:32:15 -0700

    ничего не сделал

---
+++ views/tasks/index.php
@@ -0,0 +1,55 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap_Test</title>
+
+    <!-- Bootstrap -->
+	<link rel="stylesheet" href="/math/stylesheets/bootstrap/css/bootstrap.min.css">
+	<script src="/math/scripts/jquery-3.2.0.min.js"></script>
+	<script src="/math/stylesheets/bootstrap/js/bootstrap.min.js"></script>
+	
+	</head>
+	<body>
+		<?php 
+			require_once($_SERVER['DOCUMENT_ROOT'] . '/math/templates/navbar-template.html');  
+		?>
+		
+		<div class="container-fluid" style="margin-top:4%">
+			<div class="row">
+				<div class="col-lg-offset-2 col-lg-6">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<form action="post.php" method="post">
+								<p>     
+									<b>Ваше имя:</b><br>
+									<input type="text" size="40">
+							   </p>
+</form>
+						</div>
+						<div class="panel-body">
+							
+
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-2">
+					<?php 
+						require_once($_SERVER['DOCUMENT_ROOT'] . '/math/templates/right-panel-template.html');  
+					?>
+				</div>
+				<div class="col-lg-offset-2 col-lg-8 col-md-12 col-sm-12 col-xs-12" align="center">
+					<div class="well">
+						<?php 
+							require_once($_SERVER['DOCUMENT_ROOT'] . '/math/templates/copyrights.html');  
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	</body>
+</html>
+
