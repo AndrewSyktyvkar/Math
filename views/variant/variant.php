e216a038a63dc629f46ac62bfa30d3480dffa326
BorisShostal <boroshostal@gmail.com>
2017-04-12 22:24:00 -0700

    Issue closed

--- views/variant/variant.php
+++ views/variant/variant.php
@@ -7,18 +7,10 @@
     <title>Bootstrap_Test</title>
 
     <!-- Bootstrap -->
-    
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="/math/stylesheets/bootstrap/css/bootstrap.min.css">
+	<script src="/math/scripts/jquery-3.2.0.min.js"></script>
+	<script src="/math/stylesheets/bootstrap/js/bootstrap.min.js"></script>
 	
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-		
 	</head>
 	<body>
 		<?php 
